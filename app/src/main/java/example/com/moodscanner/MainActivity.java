adc8d7187e006a26ef7091dd386dc0755f296cc4
carlosepmendes <carlosepmendes@gmail.com>
2015-07-31 16:07:57 +0000

    Added logic for the scanner

--- app/src/main/java/example/com/moodscanner/MainActivity.java
+++ app/src/main/java/example/com/moodscanner/MainActivity.java
@@ -1,38 +1,78 @@
 package example.com.moodscanner;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageView thumbPrint;
+    private TextView result;
+    private AnimationDrawable thumbAnimation;
+    private String[] moodResults;
+    private Runnable mRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moodResults = new String[]{
+                "Someone is cranky!",
+                "You are my sunshyne!",
+                "No comments...",
+                "You're stressed out!",
+                "Happy camper ;)",
+                "Not your day :(",
+                "Smile - it's good for you",
+                "In the clouds...",
+                "Passive",
+                "Sad",
+                "Excited!"
+        };
+
+        thumbPrint = (ImageView) findViewById(R.id.thumbPrint);
+        thumbPrint.setBackgroundResource(R.drawable.thumb_animation);
+        thumbAnimation = (AnimationDrawable)thumbPrint.getBackground();
+
+        result = (TextView) findViewById(R.id.resultText);
+
+        thumbPrint.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                thumbAnimation.start();
+                showResult();
+                //Toast.makeText(getApplicationContext(), "Hellooooo there", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void showResult(){
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                int rand = (int) (Math.random() * moodResults.length);
+                result.setText(moodResults[rand]);
+
+                //stop animation
+                thumbAnimation.stop();
+            }
+        };
+
+        android.os.Handler mHandler = new android.os.Handler();
+        mHandler.postDelayed(mRunnable, 5000); // 5 seconds
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
